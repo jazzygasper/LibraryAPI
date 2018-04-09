ff13436e7374b48bf4a3c2da83b2f9715bcc561d
Jazzy Gasper <jasmina.gasper@gmail.com>
2018-04-09 11:34:55 +0100

    book update

--- src/book/book.controller.js
+++ src/book/book.controller.js
@@ -12,6 +12,14 @@ class BookController {
     })
   }
 
+  static updateBook(req, res) {
+    req.body.bookId = req.body.bookId || Service.generateId(req.body);
+    console.log("book updated");
+    Book.update({bookId: req.params.bookId}, req.body, function() {
+      res.status(200).send("book updated")
+    })
+  }
+
   static deleteBook(req, res) {
     Book.remove({bookId: req.params.bookId}, function () {
       res.status(200).send("book deleted")
--- src/book/index.js
+++ src/book/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Controller = require('./book.controller');
 
 router.post('/', Controller.addBook);
+router.put('/:bookId', Controller.updateBook);
 router.delete('/:bookId', Controller.deleteBook);
 
 module.exports = router;
